db7ad89c4163972d3b000d58ebd28180fca88034
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-09 15:20:30 +0200

    fix sonar

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -13,13 +13,15 @@ const AppFooter = () => {
         </Link>
         <div className="ml-auto flex flex-row items-center">
           <p className="text-gray-300 text-sm font-sans text-right hidden lg:block">
-            An interactive learing project from&nbsp;
+            An interactive learing project from
             <a href="https://thinkster.io/" className="text-green">
               Thinkster
             </a>
             . Code & design licensed under MIT.
           </p>
           <button
+            title="Toggle dark mode"
+            type="button"
             className="flex items-center justify-center ml-4 dark:text-white"
             onClick={() => {
               setColorScheme(!isDarkMode);
