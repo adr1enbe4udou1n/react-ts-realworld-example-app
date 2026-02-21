40513b32bdb156024c69994edf813e9fba2926a1
Adrien Beaudouin <3679080+adr1enbe4udou1n@users.noreply.github.com>
2026-02-21 17:39:42 +0100

    up

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
@@ -10,7 +9,6 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintPluginPrettierRecommended,
   {
     rules: {
